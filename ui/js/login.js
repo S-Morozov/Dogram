575ad68ce2a57a736cd5d9d6a929b618828c0464
Glatus <janipudas1@gmail.com>
2023-04-30 10:33:46 +0300

    Lisää pieniä muutoksia

--- ui/js/login.js
+++ ui/js/login.js
@@ -1,6 +1,7 @@
 'use strict';
 import { url } from '../../utils/url.js';
 const loginForm = document.querySelector('#login-form');
+const logout = document.querySelector("#logout");
 const addUserForm = document.querySelector("#add-user-form");
 var openModalBtn = document.getElementById("open-modal-btn");
 var modal = document.querySelector(".modal");
@@ -53,3 +54,34 @@ openModalBtn.addEventListener("click", function () {
 closeModalBtn.addEventListener("click", function () {
   modal.classList.remove("show-modal");
 });
+
+const token = sessionStorage.getItem("token");
+const user = sessionStorage.getItem("user");
+
+//Logout
+logout.addEventListener("click", (evt) => {
+  (async () => {
+    try {
+      const response = await fetch(url + '/auth/logout');
+      const json = await response.json();
+      console.log(json);
+      // remove token
+      sessionStorage.removeItem('token');
+      sessionStorage.removeItem('user');
+      alert('You have logged out');
+      location.href = '../index.html';
+    } catch (e) {
+      console.log(e.message);
+    }
+  })();
+});
+//Piilottaa nappeja
+if (token && user) {
+  // show the logout menu item
+  document.getElementById("logout").style.display = "block";
+  document.getElementById("open-modal-btn").style.display = "none";
+} else {
+  // hide the logout menu item
+  document.getElementById("logout").style.display = "none";
+  document.getElementById("open-modal-btn").style.display = "block";
+}
