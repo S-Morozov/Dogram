8275ba69871f8dafbdaf0679fd21959353a3e128
Glatus <janipudas1@gmail.com>
2023-04-30 09:58:54 +0300

    UI-muutoksia

--- ui/js/login.js
+++ ui/js/login.js
@@ -46,35 +46,6 @@ loginForm.addEventListener('submit', async (evt) => {
   }
 });
 
-(async () => {
-  // check sessionStorage
-  if (!sessionStorage.getItem('token') || !sessionStorage.getItem('user')) {
-    return;
-  }
-  // check if token valid
-  try {
-    const fetchOptions = {
-      headers: {
-        Authorization: 'Bearer ' + sessionStorage.getItem('token'),
-      },
-    };
-    const response = await fetch(url + '/user/token', fetchOptions);
-    if (!response.ok) {
-      location.href = '../index.html';
-    } else {
-      const json = await response.json();
-      sessionStorage.setItem('user', JSON.stringify(json.user));
-    }
-  } catch (e) {
-    console.log(e.message);
-  }
-})();
-(async () => {
-  if (sessionStorage.getItem('token') || sessionStorage.getItem('user')) {
-    location.href = 'front.html';
-  }
-})();
-
 openModalBtn.addEventListener("click", function () {
   modal.classList.add("show-modal");
 });
