53adb49df683db942c14711604b894166d5c2d3c
Glatus <janipudas1@gmail.com>
2023-05-02 23:46:46 +0300

    No nyt on jonkulainen profiilisivu

--- database/dogdb.sql
+++ database/dogdb.sql
@@ -16,12 +16,10 @@
 
 
 -- Dumping database structure for dogdb
-DROP DATABASE IF EXISTS `dogdb`;
 CREATE DATABASE IF NOT EXISTS `dogdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
 USE `dogdb`;
 
 -- Dumping structure for table dogdb.comments
-DROP TABLE IF EXISTS `comments`;
 CREATE TABLE IF NOT EXISTS `comments` (
   `content` text NOT NULL,
   `created_at` date NOT NULL,
@@ -33,13 +31,12 @@ CREATE TABLE IF NOT EXISTS `comments` (
   KEY `post_id` (`post_id`),
   CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
   CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `posts` (`post_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Dumping data for table dogdb.comments: ~0 rows (approximately)
 DELETE FROM `comments`;
 
 -- Dumping structure for table dogdb.dogs
-DROP TABLE IF EXISTS `dogs`;
 CREATE TABLE IF NOT EXISTS `dogs` (
   `dog_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` text NOT NULL,
@@ -63,7 +60,6 @@ INSERT INTO `dogs` (`dog_id`, `name`, `breed`, `age`, `gender`, `color`, `bio`, 
 	(2, 'Testidoggo', 'xd', 12, 'Male', 'blue', 'd', 'yep.png', '2023-04-23', 2);
 
 -- Dumping structure for table dogdb.follows
-DROP TABLE IF EXISTS `follows`;
 CREATE TABLE IF NOT EXISTS `follows` (
   `follow_id` int(11) NOT NULL AUTO_INCREMENT,
   `created_at` date NOT NULL,
@@ -80,7 +76,6 @@ CREATE TABLE IF NOT EXISTS `follows` (
 DELETE FROM `follows`;
 
 -- Dumping structure for table dogdb.likes
-DROP TABLE IF EXISTS `likes`;
 CREATE TABLE IF NOT EXISTS `likes` (
   `like_id` int(11) NOT NULL AUTO_INCREMENT,
   `created_at` date NOT NULL,
@@ -91,13 +86,12 @@ CREATE TABLE IF NOT EXISTS `likes` (
   KEY `user_id` (`user_id`),
   CONSTRAINT `likes_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `posts` (`post_id`),
   CONSTRAINT `likes_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=224 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Dumping data for table dogdb.likes: ~0 rows (approximately)
 DELETE FROM `likes`;
 
 -- Dumping structure for table dogdb.posts
-DROP TABLE IF EXISTS `posts`;
 CREATE TABLE IF NOT EXISTS `posts` (
   `post_id` int(11) NOT NULL AUTO_INCREMENT,
   `content` text NOT NULL,
@@ -109,15 +103,15 @@ CREATE TABLE IF NOT EXISTS `posts` (
   KEY `dog_id` (`dog_id`),
   CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
   CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`dog_id`) REFERENCES `dogs` (`dog_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=74 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table dogdb.posts: ~1 rows (approximately)
+-- Dumping data for table dogdb.posts: ~2 rows (approximately)
 DELETE FROM `posts`;
 INSERT INTO `posts` (`post_id`, `content`, `created_at`, `user_id`, `dog_id`) VALUES
-	(19, 'test', '2023-04-23', 2, 2);
+	(19, 'test', '2023-04-23', 2, 2),
+	(20, 'test', '2023-04-23', 2, 1);
 
 -- Dumping structure for table dogdb.post_media
-DROP TABLE IF EXISTS `post_media`;
 CREATE TABLE IF NOT EXISTS `post_media` (
   `media_id` int(11) NOT NULL AUTO_INCREMENT,
   `media_name` text NOT NULL,
@@ -125,16 +119,15 @@ CREATE TABLE IF NOT EXISTS `post_media` (
   PRIMARY KEY (`media_id`),
   KEY `post_id` (`post_id`),
   CONSTRAINT `post_media_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `posts` (`post_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=92 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Dumping data for table dogdb.post_media: ~2 rows (approximately)
 DELETE FROM `post_media`;
 INSERT INTO `post_media` (`media_id`, `media_name`, `post_id`) VALUES
 	(2, 'imuri.jpg', 19),
-	(3, 'mie.png', 19);
+	(4, 'pepestare.png', 20);
 
 -- Dumping structure for table dogdb.roles
-DROP TABLE IF EXISTS `roles`;
 CREATE TABLE IF NOT EXISTS `roles` (
   `role_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` text NOT NULL,
@@ -148,7 +141,6 @@ INSERT INTO `roles` (`role_id`, `name`) VALUES
 	(1, 'Normal');
 
 -- Dumping structure for table dogdb.userroles
-DROP TABLE IF EXISTS `userroles`;
 CREATE TABLE IF NOT EXISTS `userroles` (
   `user_id` int(11) NOT NULL,
   `role_id` int(11) NOT NULL,
@@ -158,13 +150,13 @@ CREATE TABLE IF NOT EXISTS `userroles` (
   CONSTRAINT `userroles_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `roles` (`role_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table dogdb.userroles: ~1 rows (approximately)
+-- Dumping data for table dogdb.userroles: ~2 rows (approximately)
 DELETE FROM `userroles`;
 INSERT INTO `userroles` (`user_id`, `role_id`) VALUES
-	(2, 0);
+	(2, 0),
+	(40, 1);
 
 -- Dumping structure for table dogdb.users
-DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `username` text NOT NULL,
@@ -176,15 +168,15 @@ CREATE TABLE IF NOT EXISTS `users` (
   `website` text DEFAULT NULL,
   `created_at` date NOT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table dogdb.users: ~0 rows (approximately)
+-- Dumping data for table dogdb.users: ~2 rows (approximately)
 DELETE FROM `users`;
 INSERT INTO `users` (`user_id`, `username`, `email`, `password`, `profile_image`, `bio`, `location`, `website`, `created_at`) VALUES
-	(2, 'Admin', 'admin@metropolia.fi', '$2a$10$nBCPrxk.jG1BfQCe0L1JgOy4BFCSaf87wNyAfDWzgWhaTevcaXMY2', 'default_profile.png', NULL, '', NULL, '2023-04-20');
+	(2, 'Admin', 'admin@metropolia.fi', '$2a$10$nBCPrxk.jG1BfQCe0L1JgOy4BFCSaf87wNyAfDWzgWhaTevcaXMY2', 'default_profile.png', NULL, '', NULL, '2023-04-20'),
+	(40, 'Normal', 'test@test.com', '$2a$10$kQTNtiO2jVZsMmSBA4U3ie4xvR96k0Ycv/Ae3e6JxFJQAuJCmbzlu', 'ru4azspi85a01.jpg', NULL, '', NULL, '2023-04-30');
 
 -- Dumping structure for trigger dogdb.insert_user_role
-DROP TRIGGER IF EXISTS `insert_user_role`;
 SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO';
 DELIMITER //
 CREATE TRIGGER insert_user_role
@@ -196,14 +188,6 @@ BEGIN
 END//
 DELIMITER ;
 SET SQL_MODE=@OLDTMP_SQL_MODE;
-
--- Dumping structure for view dogdb.post_details
-DROP VIEW IF EXISTS `post_details`;
--- Removing temporary table and create final VIEW structure
-DROP TABLE IF EXISTS `post_details`;
-CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `post_details` AS SELECT posts.post_id, content, created_at, user_id, dog_id, media_name, media_id
-FROM posts
-LEFT JOIN post_media ON posts.post_id = post_media.post_id ;
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
--- ui/js/profile.js
+++ ui/js/profile.js
@@ -1,30 +1,75 @@
 import { url } from '../../utils/url.js';
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get("id");
-console.log("Id on :", id);
-const galleryContainer = document.querySelector('.gallery');
 
+//Hakee käyttäjän tiedot tokenista
+const getUserinfo = async () => {
+    const sessionToken = sessionStorage.getItem('user');
+    const user = JSON.parse(sessionToken);
+    return user;
+};
+var user = await getUserinfo();
+console.log(user);
+
+const galleryContainer = document.querySelector('.gallery');
 const fetchOptions = {
     headers: {
         Authorization: 'Bearer ' + sessionStorage.getItem('token'),
     }
 };
-const response = await fetch(url + '/post/' + id, fetchOptions);
-const posts = await response.json();
-if (posts.length > 0) {
-    console.log("Yeah boy");
-}
-const galleryHTML = galleryItems.map(item => `
-    <div class="gallery-item" tabindex="0">
-        <img src="${item.imageUrl}" class="gallery-image" alt="${item.imageAlt}">
-        <div class="gallery-item-info">
-            <ul>
-                <li class="gallery-item-likes"><span class="visually-hidden">Likes:</span><i class="fas fa-heart" aria-hidden="true"></i> ${item.likes}</li>
-                <li class="gallery-item-comments"><span class="visually-hidden">Comments:</span><i class="fas fa-comment" aria-hidden="true"></i> ${item.comments}</li>
-            </ul>
+//Haetaan käyttäjän postaamat kuvat
+const response = await fetch(url + '/post/user/' + id, fetchOptions);
+const galleryItems = await response.json();
+galleryItems.forEach(async (post) => {
+    const response2 = await fetch(url + `/comment/${post.post_id}`, fetchOptions);
+    const comments = await response2.json();
+    const response3 = await fetch(url + `/like/${post.post_id}`, fetchOptions);
+    const likes = await response3.json();
+    const response = await fetch(url + `/post/media/${post.post_id}`, fetchOptions);
+    const images = await response.json();
+    console.log(images);
+    const html = images.map(image => `
+        <div class="gallery-item" tabindex="0">
+            <img src="../../uploads/${image.media_name}" class="gallery-image" alt="${image.media_name}">
+            <div class="gallery-item-info">
+                <ul>
+                    <li class="gallery-item-likes"><span class="visually-hidden">Likes:</span><i class="fas fa-heart" aria-hidden="true"></i> ${likes}</li>
+                    <li class="gallery-item-comments"><span class="visually-hidden">Comments:</span><i class="fas fa-comment" aria-hidden="true"></i> ${comments.length}</li>
+                </ul>
+            </div>
         </div>
-    </div>
-`).join('');
+    `).join('');
+    galleryContainer.innerHTML += html;
+});
 
-// Add the generated HTML to the gallery container
-galleryContainer.innerHTML = galleryHTML;
+const container = document.querySelector('.container');
+
+//Feikki dataa ei ollut aikaa
+const followerCount = 188;
+//Feikki dataa ei ollut aikaa
+const followingCount = 206;
+
+const html = `
+  <div class="profile">
+    <div class="profile-image">
+      <img src="../../uploads/${user.profile_image}" alt="">
+    </div>
+    <div class="profile-user-settings">
+      <h1 class="profile-user-name">${user.username}</h1>
+      <button class="btn profile-edit-btn">Edit Profile</button>
+      <button class="btn profile-settings-btn" aria-label="profile settings"><i class="fas fa-cog" aria-hidden="true"></i></button>
+    </div>
+    <div class="profile-stats">
+      <ul>
+        <li><span class="profile-stat-count">${galleryItems.length}</span> posts</li>
+        <li><span class="profile-stat-count">${followerCount}</span> followers</li>
+        <li><span class="profile-stat-count">${followingCount}</span> following</li>
+      </ul>
+    </div>
+    <div class="profile-bio">
+      <p><span class="profile-real-name">${user.username}</span> ${user.bio}</p>
+    </div>
+  </div>
+`;
+
+container.innerHTML = html;
